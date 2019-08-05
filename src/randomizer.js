84d6516e4f0d2b95cdb828587966befea5d4aeee
pixelgoo <pixelgoo@gmail.com>
2019-08-05 22:58:30 +0300

    Not using randomProperty()

--- src/randomizer.js
+++ src/randomizer.js
@@ -1,8 +1,3 @@
 export function randomInteger(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-
-export function randomProperty(obj) {
-  const keys = Object.keys(obj)
-  return obj[keys[keys.length * Math.random() << 0]];
-}
