3163e1af6e006705d61ef3efac9d67a81b42292a
pixelgoo <pixelgoo@gmail.com>
2019-08-08 19:24:12 +0300

    Add commenst to getHighlightedSlots function

--- src/functions/getHighlightedSlots.js
+++ src/functions/getHighlightedSlots.js
@@ -11,9 +11,13 @@ export default function getHighlightedSlots(state) {
   slotPlacementConfig.forEach(placement => highlightedSlots[placement] = []);
 
   if (state.activeWinConditions !== null) {
+    // Check every win condition
     state.winConditions.forEach(winCondition => {
+      // For active win condition
       if (state.activeWinConditions.includes(winCondition.id)) {
+        // Checking every placement
         slotPlacementConfig.forEach(placement => {
+          // For COMBINATION_ANY_LINE condition
           if(winCondition.isCombination) {            
             winCondition.slots.forEach(slot => {
               if (state.machineState[placement].includes(slot) && 
@@ -21,6 +25,7 @@ export default function getHighlightedSlots(state) {
                 highlightedSlots[placement].push(slot)
               }
             })
+          // For other conditions
           } else {
             if (winCondition.slots.every(slot => state.machineState[placement].includes(slot))) {
               winCondition.slots.forEach(slot => highlightedSlots[placement].push(slot))
